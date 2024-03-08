9240f61fb417a11f53f53ad144643a6b0bd1626a
Deepgavade <deepakgavade0458@gmail.com>
2024-03-08 16:42:39 +0530

    deleted auth

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require( 'express' );
-const controllers = require('../controllers/auth')
-
-const router = express.Router();
-
-router.post( '/regester',  controllers.regester) 
-
-
-module.exports = router;
